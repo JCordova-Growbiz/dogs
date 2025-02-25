3db19049ac5041b76aaa96eecdef4517284a96d6
Jecy2305 <Edu_Cordova1@hotmail.com>
2025-02-25 00:19:24 -0500

    update hero imgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import blob from '../assets/imgs/blob.png'
+import dog from '../assets/imgs/dog.png'
 
 function Hero() {
   return (
@@ -27,20 +28,43 @@ function Hero() {
                 </form>
             </div>
 
-            <div className="hero-blob">
+            {/* <div className="hero-blob">
                 <img src={blob} alt=""/>
-            </div>
+            </div> */}
 
         </div>
 
         <div className="hero-main">
             <div className="hero-main-texts">
 
+                <div className="hero-main-texts-container">
+                    
+                        <div className="hero-main-texts-title">
+                            <p className='hero-title'>One More Friend</p >
+                            <p className='hero-subtitle'>Thousands Moru Fun!</p>
+                        </div>
+
+                        <div className="hero-main-texts-subtitle">
+                            <div className="hero-container-subtitle">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex excepturi magni tempore quidem nesciunt odio, consequatur voluptatum unde.</p>
+                            </div>
+                        </div>
+
+                        <div className="hero-main-texts-buttons">
+                            <button className='hero-button-intro'>View Intro</button>
+                            <button className='hero-button-now'>Explore Now</button>
+                        </div>
+                </div>
+                    
             </div>
 
             <div className="hero-main-imgs">
                 <div className="box-blue"></div>
-                <div className="box-cream"></div>
+                <div className="box-cream">
+                    <div className="box-cream-container">
+                        <img className='box-dog' src={dog} alt="dog" />
+                    </div>
+                </div>
             </div>
         </div>
         
